2b389864d038c7e02f92076fd6188238f2bc7e2e
Clistenes Araujo <clistenes.araujo@lavid.ufpb.br>
2021-02-27 11:58:59 -0300

    adding networks, dependencies and connecting database

--- app/sender.py
+++ app/sender.py
@@ -1,4 +1,19 @@
+import psycopg2
 from bottle import route, run, request
+
+DSN = 'dbname=email_sender user=example host=db password=123456'
+SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
+
+
+def register_message(subject, message):
+    conn = psycopg2.connect(DSN)
+    cur = conn.cursor()
+    cur.execute(SQL, (subject, message))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print('Recorded Message!')
 
 
 @route('/', method='POST')
@@ -6,6 +21,9 @@ def send():
     email = request.forms.get('email')
     message = request.forms.get('message')
     subject = request.forms.get('subject')
+
+    register_message(subject, message)
+
     return 'Enviando Msg!!! email: {} subject: {} Messagem: {}'.format(
         email, subject, message
     )
